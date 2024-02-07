dea1d35252641a9bbebb4c07a3b6c96ef08c3b5c
Joe Rollerson <joerollerson@gmail.com>
2024-02-07 10:58:49 +0000

    change port

--- app.js
+++ app.js
@@ -232,4 +232,6 @@ app.post("/sign-up", [
     })
   );
 
-  app.listen(process.env.PORT, () => console.log("app listening on port 3000!"));
+  const port = process.env.PORT || 3000
+
+  app.listen(port, () => console.log(`app listening on port ${port}`));
