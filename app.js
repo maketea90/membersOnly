27f6dd524fd22539e5a07157b02e725bcfada065
Joe Rollerson <joerollerson@gmail.com>
2024-02-07 11:13:30 +0000

    add hostname

--- app.js
+++ app.js
@@ -234,4 +234,4 @@ app.post("/sign-up", [
 
   const port = process.env.PORT || 3000
 
-  app.listen(port, () => console.log(`app listening on port ${port}`));
+  app.listen(port, "0.0.0.0", () => console.log(`app listening on port ${port}`));
